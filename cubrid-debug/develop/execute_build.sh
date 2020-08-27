5f38d7ba7ece65cefcef55992c4f9cee5d8fc30d
Youngjin Joo <youngjinj@cubrid.com>
2020-08-27 11:17:46 +0900

    Rebuild after modifying the source code, and modify the script to update ctags and cscope DB.

--- cubrid-debug/develop/execute_build.sh
+++ cubrid-debug/develop/execute_build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cubrid service stop
+
+rm -rf /home/cubrid/CUBRID
+
 cd /home/cubrid/github/cubrid \
 	&& ./build.sh -m debug -p /home/cubrid/CUBRID -j /usr/lib/jvm/java build \
 	&& echo 'java_stored_procedure=y' >> /home/cubrid/CUBRID/conf/cubrid.conf \
@@ -7,9 +11,11 @@ cd /home/cubrid/github/cubrid \
 		/home/cubrid/CUBRID/tmp \
 		/home/cubrid/CUBRID/var/CUBRID_SOCK
 
+. /home/cubrid/cubrid.sh
+
 mkdir -p $CUBRID_DATABASES/demodb \
-	&& createdb -F $CUBRID_DATABASES/demodb demodb ko_KR.utf8
-	&& cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects -v demodb
-	&& sed s/#server=foo,bar/server=demodb/g -i \$CUBRID/conf/cubrid.conf
+	&& cubrid createdb -F $CUBRID_DATABASES/demodb demodb ko_KR.utf8 \
+	&& cubrid loaddb -u dba -s $CUBRID/demo/demodb_schema -d $CUBRID/demo/demodb_objects -v demodb \
+	&& sed s/#server=foo,bar/server=demodb/g -i $CUBRID/conf/cubrid.conf
 
 cubrid service restart
